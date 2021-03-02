f50136e6f89749953afc031ba47d312d64240f03
Emirhan <emirhanaydin.imyo20@iste.edu.tr>
2021-03-02 23:56:16 +0300

    Create Yazinini_Kac_Karakter_Oldugunu_Bulan_Prog.cpp

---
+++ Yazinini_Kac_Karakter_Oldugunu_Bulan_Prog.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main(){
+	
+	char yazi[80];
+	
+	cout<<"Bir seyler yazin : ";
+	cin>>yazi;
+	cout<<"Girilen yazinin karakter sayisi : "<<strlen(yazi);
+	
+}
